81dfde0362e30d7e99c84d083f72b8951b79ad93
ronan <ronanhenry@orange.fr>
2022-01-04 17:28:12 +0100

    interface du site

---
+++ index.php
@@ -0,0 +1,72 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-SHOP</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/6ca76ec2b7.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <!-- Barre de navigation -->
+    <nav>
+      <h1>E-SHOP</h1>
+      <div class="onglets">
+        <p>Homme</p>
+        <p>Femme</p>
+        <p>Enfant</p>
+        <form>
+          <input type="search" placeholder="Rechercher">
+        </form>
+        <p><i class="far fa-heart"></i></p>
+        <p><i class="fas fa-shopping-cart"></i></p>
+      </div>
+    </nav>
+
+    <!-- Titre -->
+    <header>
+      <h1>Voici notre boutique, votre chez-vous.</h1>
+      <button>Naviguer <i class="fas fa-paper-plane"></i></button>
+    </header>
+
+    <!-- Articles -->
+    <section class="main">
+      <div class="articles">
+        <div class="article">
+          <img src="shortNike.jpg" alt="">
+          <div class="article-header">
+            <h4 class="titre">Short Nike</h4>
+            <h4 class="prix">199€</h4>
+          </div>
+          <div class="article-body">
+            <p>Short moulant pour homme</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="main">
+      <div class="articles">
+        <div class="article">
+          <img src="shortNike.jpg" alt="">
+          <div class="article-header">
+            <h4 class="titre">Short Nike</h4>
+            <h4 class="prix">199€</h4>
+          </div>
+          <div class="article-body">
+            <p>Short moulant pour homme</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+      <p>&copy; Contactez-nous au 06 55 23 14 95</p>
+      <div class="social-media">
+        <p><i class="fab fa-facebook-f"></i></p>
+        <p><i class="fab fa-twitter"></i></p>
+        <p><i class="fab fa-instagram"></i></p>
+        <p><i class="fab fa-linkedin-in"></i></p>
+      </div>
+    </footer>
+  </body>
+</html>
